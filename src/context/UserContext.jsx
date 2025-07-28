2aaa50b8c635ebe85815601d597de73dddaf55aa
Sai Harsha Gangari <harshagangari747@gmail.com>
2025-07-28 16:31:29 -0400

    resolved waiting for token error

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -43,7 +43,7 @@ export const UserProvider = ({ children }) => {
     };
 
     const waitForTokenFetch = () => {
-      console.log("Waiting for token...", token);
+      console.log("Waiting for token...");
       const token = sessionStorage.getItem("access_token");
       if (token) {
         fetchUser();
